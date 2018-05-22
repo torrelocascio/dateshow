e67c6357b7121af730b53582e4c247fde765a238
Torre <torre.locascio@gmail.com>
2018-05-22 11:05:52 -0400

    added more styling, cleaned app code by seperating components

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
-import {Navigation} from 'react-native-navigation'
+import { Navigation } from "react-native-navigation";
+import { Provider } from "react-redux";
 
-import AuthScreen from './src/screens/Auth/Auth'
+import AuthScreen from "./src/screens/Auth/Auth";
+import SharePlaceScreen from "./src/screens/SharePlace/SharePlace";
+import FindPlaceScreen from "./src/screens/FindPlace/FindPlace";
+import PlaceDetailScreen from "./src/screens/PlaceDetail/PlaceDetail";
+import configureStore from "./src/store/configureStore";
+import SideDrawer from './src/screens/SideDrawer/SideDrawer'
 
-//Register Screens
-Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
+const store = configureStore();
 
-//Start a App
+// Register Screens
+Navigation.registerComponent(
+  "awesome-places.AuthScreen",
+  () => AuthScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.SharePlaceScreen",
+  () => SharePlaceScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.FindPlaceScreen",
+  () => FindPlaceScreen,
+  store,
+  Provider
+);
+Navigation.registerComponent(
+  "awesome-places.PlaceDetailScreen",
+  () => PlaceDetailScreen,
+  store,
+  Provider
+)
 
+
+Navigation.registerComponent(
+  "awesome-places.SideDrawer",
+  () => SideDrawer 
+)
+
+
+// Start a App
 Navigation.startSingleScreenApp({
   screen: {
     screen: "awesome-places.AuthScreen",
     title: "Login"
   }
-})
+});
